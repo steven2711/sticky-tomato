4a0b870ad4915c85927d09631c76028fc17c4f25
Steven Watson <swatson.emu@gmail.com>
2021-09-03 18:21:32 -0500

    Fixed build error.

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,7 @@
+export default function AdminDashboard() {
+  return (
+    <div>
+      <h1>admin dashboard</h1>
+    </div>
+  );
+}
---
+++ pages/admin/index.js
@@ -0,0 +1,7 @@
+export default function AdminLogin() {
+  return (
+    <div>
+      <h1>admin login</h1>
+    </div>
+  );
+}
